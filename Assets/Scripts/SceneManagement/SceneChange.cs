560d479db37f9dca25a359b9dc152c6651b36170
Christopher Anderson <144862713+cswanderson@users.noreply.github.com>
2024-03-07 20:43:40 -0800

    SceneChangeTry

---
+++ Assets/Scripts/SceneManagement/SceneChange.cs
@@ -0,0 +1,14 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+
+    [SerializeField] bool abc;
+
+    private class Change
+    {
+  
+    }
+
+}
