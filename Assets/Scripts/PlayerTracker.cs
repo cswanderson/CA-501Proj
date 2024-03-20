dfcc2a07dfac4a290f18690f9bc8e34380ebf78c
VEMSOK <vems.ok@ubc.ca>
2024-03-20 14:29:58 -0700

    accessing instance IDs

--- Assets/Scripts/PlayerTracker.cs
+++ Assets/Scripts/PlayerTracker.cs
@@ -1,6 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using getReal3D;
+using TMPro;
+using UnityEngine;
 
 public class PlayerTracker : SingletonMonoBehavior<PlayerTracker>
 {
+    [SerializeField] private List<TextMeshProUGUI> fields;
     
+    private void Update()
+    {
+        foreach (TextMeshProUGUI tmp in fields)
+        {
+            tmp.text = Plugin.getClusterID().ToString();
+        }
+    }
 }
